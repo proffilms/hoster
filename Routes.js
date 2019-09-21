53491b560396a46f2de0dd76a51ca6ad725b54c9
profflims <profflims@yahoo.com>
2019-09-21 06:34:49 +0000

    update with PORT

--- Routes.js
+++ Routes.js
@@ -10,7 +10,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 *******************************************/
 const express_1 = __importDefault(require("express"));
 const jimp_1 = __importDefault(require("jimp"));
-const axios_1 = __importDefault(require("axios"));
 const hostname = "https://proffilms-hosting.firebaseapp.com";
 const routes = express_1.default.Router();
 routes.get("/i", (req, res) => {
@@ -24,154 +23,146 @@ routes.get("/image/:id?", (req, res) => {
         myHostname = req.query.l;
     else
         myHostname = hostname + "/files/D2.jpg";
-    axios_1.default({
-        url: myHostname,
-        method: "GET",
-        responseType: "arraybuffer"
-    }).then(data => {
-        jimp_1.default.read(data.data).then((image) => {
-            let width = jimp_1.default.AUTO, height = jimp_1.default.AUTO, limit = 120;
-            const initWidth = image.bitmap.width, initHeight = image.bitmap.height;
-            // CHECK ALL QUERIES - l,w,h,
-            if (req.query.limit && req.query.limit !== "") {
-                // IF LIMIT IS SET
-                limit = req.query.limit;
-                if (image.bitmap.width >= image.bitmap.height) {
-                    width = limit;
-                    height = jimp_1.default.AUTO;
-                }
-                else {
-                    width = jimp_1.default.AUTO;
-                    height = limit;
-                }
+    jimp_1.default.read(myHostname).then((image) => {
+        let width = jimp_1.default.AUTO, height = jimp_1.default.AUTO, limit = 120;
+        const initWidth = image.bitmap.width, initHeight = image.bitmap.height;
+        // CHECK ALL QUERIES - l,w,h,
+        if (req.query.limit && req.query.limit !== "") {
+            // IF LIMIT IS SET
+            limit = req.query.limit;
+            if (image.bitmap.width >= image.bitmap.height) {
+                width = limit;
+                height = jimp_1.default.AUTO;
             }
-            else if ((req.query.w || +req.query.w === 0) && (req.query.h || +req.query.h === 0)) {
-                // IF WIDTH & HEIGHT ARE BOTH SET
-                width = req.query.w.toLowerCase() === "auto" ? jimp_1.default.AUTO : req.query.w !== "" ? +req.query.w : limit;
-                height = req.query.h.toLowerCase() === "auto" ? jimp_1.default.AUTO : req.query.h !== "" ? +req.query.h : limit;
-                width = +width === 0 ? 1 : width;
-                height = +height === 0 ? 1 : height;
-                if (req.query.w.toLowerCase() === "auto" && req.query.h.toLowerCase() === "auto")
-                    width = image.bitmap.width;
-                // FIRST CHECK FOR THE LONGEST SIDE AND RESIZE USING THAT SIDE
-                if (image.bitmap.width >= image.bitmap.height) {
-                    // HORIZONTAL OR SQUARE IMAGE
-                    if (width >= height)
-                        image.resize(jimp_1.default.AUTO, width);
-                    else
-                        image.resize(jimp_1.default.AUTO, height);
-                }
-                else {
-                    // VERTICAL IMAGE
-                    if (width < height)
-                        image.resize(height, jimp_1.default.AUTO);
-                    else
-                        image.resize(width, jimp_1.default.AUTO);
-                }
-                req.query.w.toLowerCase() === "auto" || req.query.h.toLowerCase() === "auto" ? null : image.crop(jimp_1.default.HORIZONTAL_ALIGN_CENTER, 0, +width, +height);
-                console.log("Initial:", initWidth + " * " + initHeight, "Tempered:", image.bitmap.width + " * " + image.bitmap.height);
+            else {
+                width = jimp_1.default.AUTO;
+                height = limit;
             }
-            else if (!req.query.w && !req.query.h) {
-                // IF W & H ARE NOT SET
-                if (image.bitmap.width >= image.bitmap.height) {
-                    width = limit;
-                    height = jimp_1.default.AUTO;
-                }
-                else {
-                    width = jimp_1.default.AUTO;
-                    height = limit;
-                }
-            }
-            else if (req.query.w) {
-                // IF WIDTH IS ONLY SET
-                width = req.query.w.toLowerCase() === "auto" ? jimp_1.default.AUTO : req.query.w;
-                height = req.query.w.toLowerCase() === "auto" ? image.bitmap.height : jimp_1.default.AUTO;
-            }
-            else if (req.query.h) {
-                // IF HEIGHT IS ONLY SET
-                width = req.query.h.toLowerCase() === "auto" ? image.bitmap.width : jimp_1.default.AUTO;
-                height = req.query.h.toLowerCase() === "auto" ? jimp_1.default.AUTO : req.query.h;
-            }
-            // FIRST CHECK IF THE FILENAME EXISTS ---------------------------------
-            // let ext = image.getExtension();
-            // let filename = req.query.name;
-            // let thePath = path.join(__dirname, filename + "." + ext);
-            // CHECK ALL QUERIES - b,g,q
-            if (req.query.b)
-                image.blur(req.query.b !== "" ? +req.query.b : 5);
-            // if(req.query.g) image.greyscale();
-            if (req.query.g)
-                image.color([
-                    { apply: 'desaturate', params: [+req.query.g] }
-                ]);
-            if (req.query.q)
-                image.quality(req.query.q !== "" ? +req.query.q : 75);
-            // image.contain(+width, +height, Jimp.HORIZONTAL_ALIGN_LEFT | Jimp.VERTICAL_ALIGN_TOP);
-            image.resize(+width, +height);
-            image.getBase64(jimp_1.default.AUTO, (err, base) => {
-                // console.log("The Base64 is: ",base);
-                if (err)
-                    res.status(400).json({ error: "Base64 image Processing failed. Sorry, There's seems to be an error" });
-                // res.send(base);
-                const sendJSON = {
-                    image: base,
-                    initWidth: initWidth,
-                    initHeight: initHeight,
-                    width: image.bitmap.width,
-                    height: image.bitmap.height,
-                    ext: image.getExtension(),
-                };
-                if (req.params.id) {
-                    /*  let r = fs.createReadStream(sendJSON.image);
-                     let ps = new stream.PassThrough();
-                     stream.pipeline(r,ps,(err:any) => {
-                       if(err) console.log(err);
-                       return res.sendStatus(400);
-                     });
-                     ps.pipe(res); */
-                    // .replace(/^data:[a-zA-Z]+\/[a-zA-Z]+;base64,/, '')
-                    const img = Buffer.from(sendJSON.image.replace(/^data:[a-zA-Z]+\/[a-zA-Z]+;base64,/, ''), 'base64');
-                    res.writeHead(202, {
-                        'Content-Type': 'image/' + sendJSON.ext,
-                        'Content-Length': img.length
-                    });
-                    res.end(img);
-                    // res.send(sendJSON.image);
-                }
+        }
+        else if ((req.query.w || +req.query.w === 0) && (req.query.h || +req.query.h === 0)) {
+            // IF WIDTH & HEIGHT ARE BOTH SET
+            width = req.query.w.toLowerCase() === "auto" ? jimp_1.default.AUTO : req.query.w !== "" ? +req.query.w : limit;
+            height = req.query.h.toLowerCase() === "auto" ? jimp_1.default.AUTO : req.query.h !== "" ? +req.query.h : limit;
+            width = +width === 0 ? 1 : width;
+            height = +height === 0 ? 1 : height;
+            if (req.query.w.toLowerCase() === "auto" && req.query.h.toLowerCase() === "auto")
+                width = image.bitmap.width;
+            // FIRST CHECK FOR THE LONGEST SIDE AND RESIZE USING THAT SIDE
+            if (image.bitmap.width >= image.bitmap.height) {
+                // HORIZONTAL OR SQUARE IMAGE
+                if (width >= height)
+                    image.resize(jimp_1.default.AUTO, width);
                 else
-                    res.json(sendJSON);
-                // console.log(JSON.parse(image));
-                /* if(fs.existsSync(thePath)) {
-                  // IF IT ENDS WITH A NUMBER
-                  let pattern = /[\d]+$/;
-                  let num = 1, count = 0;
-                  // KEEP CHECKING IF IT ENDS WITH A NUMBER --------------------------------------
-                  while(fs.existsSync(thePath) && count < 30) {
-                    let objNum = thePath.replace("." + ext , "").match(pattern);
-                    num = objNum ? (+objNum[0] + 1) : 1;
-                    console.log("num: ", num);
-                    thePath = path.join(__dirname, filename + "~" +num + "." + ext);
-                    count ++;
-                  }
-                }else {
-                  console.log("Name doesn't exist");
-                } */
-                // res.json(image);
-                /* res.send(`
-                <div style="position: relative; display:flex; flex-flow:column wrap; align-items: center;">
-                  <p style="flex: 0 0 30px;">This is the image converted to base64</p>
-                  <p style="flex: 0 0 30px;">${ filename } - ${ image.bitmap.width } * ${ image.bitmap.height } ${ image.getExtension() }</p>
-                  <div style="position: relative; overflow: hidden; padding: 40px; flex: 1 0;">
-                    <img style="height: 100%" src="${base}"/>
-                  </div>
-                </div>
-                `); */
-            });
-        }).catch((err) => {
-            res.status(res.statusCode).json({ error: "There seems to be an error with the image! " + err });
+                    image.resize(jimp_1.default.AUTO, height);
+            }
+            else {
+                // VERTICAL IMAGE
+                if (width < height)
+                    image.resize(height, jimp_1.default.AUTO);
+                else
+                    image.resize(width, jimp_1.default.AUTO);
+            }
+            req.query.w.toLowerCase() === "auto" || req.query.h.toLowerCase() === "auto" ? null : image.crop(jimp_1.default.HORIZONTAL_ALIGN_CENTER, 0, +width, +height);
+            console.log("Initial:", initWidth + " * " + initHeight, "Tempered:", image.bitmap.width + " * " + image.bitmap.height);
+        }
+        else if (!req.query.w && !req.query.h) {
+            // IF W & H ARE NOT SET
+            if (image.bitmap.width >= image.bitmap.height) {
+                width = limit;
+                height = jimp_1.default.AUTO;
+            }
+            else {
+                width = jimp_1.default.AUTO;
+                height = limit;
+            }
+        }
+        else if (req.query.w) {
+            // IF WIDTH IS ONLY SET
+            width = req.query.w.toLowerCase() === "auto" ? jimp_1.default.AUTO : req.query.w;
+            height = req.query.w.toLowerCase() === "auto" ? image.bitmap.height : jimp_1.default.AUTO;
+        }
+        else if (req.query.h) {
+            // IF HEIGHT IS ONLY SET
+            width = req.query.h.toLowerCase() === "auto" ? image.bitmap.width : jimp_1.default.AUTO;
+            height = req.query.h.toLowerCase() === "auto" ? jimp_1.default.AUTO : req.query.h;
+        }
+        // FIRST CHECK IF THE FILENAME EXISTS ---------------------------------
+        // let ext = image.getExtension();
+        // let filename = req.query.name;
+        // let thePath = path.join(__dirname, filename + "." + ext);
+        // CHECK ALL QUERIES - b,g,q
+        if (req.query.b)
+            image.blur(req.query.b !== "" ? +req.query.b : 5);
+        // if(req.query.g) image.greyscale();
+        if (req.query.g)
+            image.color([
+                { apply: 'desaturate', params: [+req.query.g] }
+            ]);
+        if (req.query.q)
+            image.quality(req.query.q !== "" ? +req.query.q : 75);
+        // image.contain(+width, +height, Jimp.HORIZONTAL_ALIGN_LEFT | Jimp.VERTICAL_ALIGN_TOP);
+        image.resize(+width, +height);
+        image.getBase64(jimp_1.default.AUTO, (err, base) => {
+            // console.log("The Base64 is: ",base);
+            if (err)
+                res.status(400).json({ error: "Base64 image Processing failed. Sorry, There's seems to be an error" });
+            // res.send(base);
+            const sendJSON = {
+                image: base,
+                initWidth: initWidth,
+                initHeight: initHeight,
+                width: image.bitmap.width,
+                height: image.bitmap.height,
+                ext: image.getExtension(),
+            };
+            if (req.params.id) {
+                /*  let r = fs.createReadStream(sendJSON.image);
+                 let ps = new stream.PassThrough();
+                 stream.pipeline(r,ps,(err:any) => {
+                   if(err) console.log(err);
+                   return res.sendStatus(400);
+                 });
+                 ps.pipe(res); */
+                // .replace(/^data:[a-zA-Z]+\/[a-zA-Z]+;base64,/, '')
+                const img = Buffer.from(sendJSON.image.replace(/^data:[a-zA-Z]+\/[a-zA-Z]+;base64,/, ''), 'base64');
+                res.writeHead(202, {
+                    'Content-Type': 'image/' + sendJSON.ext,
+                    'Content-Length': img.length
+                });
+                res.end(img);
+                // res.send(sendJSON.image);
+            }
+            else
+                res.json(sendJSON);
+            // console.log(JSON.parse(image));
+            /* if(fs.existsSync(thePath)) {
+              // IF IT ENDS WITH A NUMBER
+              let pattern = /[\d]+$/;
+              let num = 1, count = 0;
+              // KEEP CHECKING IF IT ENDS WITH A NUMBER --------------------------------------
+              while(fs.existsSync(thePath) && count < 30) {
+                let objNum = thePath.replace("." + ext , "").match(pattern);
+                num = objNum ? (+objNum[0] + 1) : 1;
+                console.log("num: ", num);
+                thePath = path.join(__dirname, filename + "~" +num + "." + ext);
+                count ++;
+              }
+            }else {
+              console.log("Name doesn't exist");
+            } */
+            // res.json(image);
+            /* res.send(`
+            <div style="position: relative; display:flex; flex-flow:column wrap; align-items: center;">
+              <p style="flex: 0 0 30px;">This is the image converted to base64</p>
+              <p style="flex: 0 0 30px;">${ filename } - ${ image.bitmap.width } * ${ image.bitmap.height } ${ image.getExtension() }</p>
+              <div style="position: relative; overflow: hidden; padding: 40px; flex: 1 0;">
+                <img style="height: 100%" src="${base}"/>
+              </div>
+            </div>
+            `); */
         });
-    }).catch(err => {
-        res.status(401).send(err);
+    }).catch((err) => {
+        res.status(res.statusCode).json({ error: "There seems to be an error with the image! " + err });
     });
 });
 // 404 HANDLER -------------------------------
